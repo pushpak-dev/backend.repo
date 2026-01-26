2bd23f64b3639ea98c2e356d66dab1418b07e4b1
pushpak-dev <pushpak90208@gmail.com>
2026-01-26 10:59:35 +0530

    added day5 folder

---
+++ day5/src/app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+
+
+const app = express();
+
+app.use(express.json())
+
+const notes=[];
+
+app.post('/notes',(req,res)=>{
+       console.log(req.body)
+       notes.push(req.body)
+       res.status(201).json({
+              message:"Note created successfully"
+       })
+})
+app.get('/notes',(req,res)=>{
+       res.status(200).json({
+              notes:notes
+       })
+})
+app.delete('/notes/:index',(req,res)=>{
+        delete notes[req.params.index]
+        res.status(204).json({
+              message:"Note deleted Successfully"
+        })
+})
+app.patch('/notes/:id',(req,res)=>{
+       notes[req.params.id].description= req.body.description
+
+       res.status(200).json({
+              message:"Notes updated successfully"
+       })
+})
+
+module.exports=app;
